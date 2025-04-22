fb3e49cdcbc441c887d31749f0b9929787b25db6
Ma1ik-111 <gameakplay@gmail.com>
2025-04-22 21:09:48 +0300

    import axios

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -1,2 +1,21 @@
+import axios from 'axios';
+import {
+  INIT_FETCH_CONFIG,
+  DEFAULT_QUERY_PARAMS_FOR_SEARCH,
+} from 'utils/constants';
+
 const tmdb = axios.create(INIT_FETCH_CONFIG);
-1
+let apiConfigDetails = null;
+
+async function getApiConfigDetails() {
+  const url = '/configuration';
+  try {
+    const response = await tmdb(url);
+    apiConfigDetails = response.data;
+    return response.data;
+  } catch (error) {
+    console.error(`Error in getting of API configuration:${error}`);
+  }
+}
+
+getApiConfigDetails();
